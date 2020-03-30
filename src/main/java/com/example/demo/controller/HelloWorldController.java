08c128be020fe04575363039d298b7272a08b203
pingping260687005 <pingping_01@hotmail.com>
2020-03-30 17:01:00 +0800

    test1

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorldController {
     @RequestMapping("/hello")
+    @ResponseBody
     public String index() {
         return "Hello World";
     }
